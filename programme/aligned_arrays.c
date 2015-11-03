32c207791291f6c7beb37b56d1f4ec7ca738ffda
YarnSeemannsgarn <janschlenker1990@gmail.com>
2015-11-03 10:49:07 +0100

    Add measuring time

--- programme/aligned_arrays.c
+++ programme/aligned_arrays.c
@@ -1,13 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #define max 1024 * 1024 // Use ulimit -s 65536 in bash to enable big enough stack
 
 int main(){
    float a[max], b[max], c[max], d[max];
-
+   
    int i;
+   clock_t begin, end;
+   double time_spent;
+   printf("Starting to traverse 1024x1024 arrays\n");
+   begin = clock();
    for(i = 0; i < max; i++)
      a[i] = b[i] + c[i] * d[i];
+   end = clock();
+   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+   printf("Time spent: %fs\n", time_spent);  
 
    return EXIT_SUCCESS;
 }
--- programme/aligned_arrays_optimized.c
+++ programme/aligned_arrays_optimized.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #define max 1024 * 1024 // Use ulimit -s 65536 in bash to enable big enough stack
 
@@ -6,8 +8,15 @@ int main(){
   float a[max * 4]; // Use only one array to reduce cache misses
 
   int i;
+  clock_t begin, end;
+  double time_spent;
+  printf("Starting to traverse 1024x1024 arrays\n");
+  begin = clock();
   for(i = 0; i < max; i++)
     a[i*4] = a[i*4+1] + a[i*4+2] * a[i*4+3];
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Time spent: %fs\n", time_spent);
 
-   return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
